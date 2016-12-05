a1edf8243929754909c11125da5a92ed3fa51542
Abby Moore <abbyleighanne@gmail.com>
2016-12-05 11:26:56 -0600

    part 3 routing

--- app/app.js
+++ app/app.js
@@ -1,3 +1,14 @@
 "use strict";
 
-var app = angular.module("BookApp", []);
+var app = angular.module("BookApp", ['ngRoute']);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/guide-list.html',
+        controller: 'bookCtrl'
+      })
+      .otherwise('/');
+  }
+]);
